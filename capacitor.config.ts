0faee42d7a8b864e061d8a9a90f2596ab39c3727
joemafu <joe_mafu@hotmail.com>
2024-09-17 01:44:12 -0300

    Primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'alarma.de.robo',
   appName: 'AlarmaDeRobo',
   webDir: 'www'
 };
